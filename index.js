e56b72aeb365fd3721ba460ce5a900be16443b2b
Simon Støvring <mail@simonbs.dk>
2017-01-15 20:33:45 +0100

    Uses roomba module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require("request");
+var Roomba = require("roomba");
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
@@ -10,9 +10,7 @@ module.exports = function(homebridge) {
 function RoombaAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
-  this.blid = config["blid"];
-  this.robotpwd = config["robotpwd"];
-  this.assetId = config["assetid"];
+  this.roomba = new Roomba(config["blid"], config["robotpwd"], config["assetid"]);
 }
 
 RoombaAccessory.prototype.getServices = function() {
@@ -28,99 +26,14 @@ RoombaAccessory.prototype.identify = function(callback) {
   callback();
 }
 
-RoombaAccessory.prototype.sendRequest = function(method, value, callback) {
-  var form = {
-    "blid": this.blid,
-    "robotpwd": this.robotpwd,
-    "method": method
-  }
-  if (value != null) {
-    form["value"] = JSON.stringify(value);
-  }
-  request.post({
-    url: "https://irobot.axeda.com/services/v1/rest/Scripto/execute/AspenApiRequest", 
-    form: form,
-    headers: {
-      "ASSET-ID": this.assetId
-    }
-  }, callback);
-}
-
-RoombaAccessory.prototype.getStatus = function(callback) {
-  var accessory = this;
-  this.sendRequest("getStatus", null, function(err, httpResponse, body) {
-    if (err || httpResponse.statusCode != 200) {
-      accessory.log("Could not get status from Roomba: %s", err);
-      return callback(err, null);
-    }
-    var json = JSON.parse(body);
-    var theStatus = json;
-    theStatus["robot_status"] = JSON.parse(json["robot_status"]);
-    callback(null, theStatus);
-  });
-}
-
-RoombaAccessory.prototype.pollPhase = function(desiredPhase, callback) {
-  var accessory = this;
-  this.getStatus(function(err, theStatus) {
-    if (err) {
-      return callback(err);
-    }
-    if (theStatus.robot_status.phase == desiredPhase) {
-      return callback(null);
-    }
-    setTimeout(function() {
-      accessory.pollPhase(desiredPhase, callback);
-    }, 3000);
-  });
-}
-
-RoombaAccessory.prototype.pauseAndDock = function(callback) {
-  var accessory = this;
-  this.sendRequest("multipleFieldSet", {"remoteCommand": "pause"}, function(err, httpResponse, body) {
-    if (err || httpResponse.statusCode != 200) {
-      accessory.log("Could not pause Roomba: %s", err);
-      return callback(err);
-    }
-    accessory.pollPhase("stop", function(err) {
-      if (err) {
-        return callback(err);
-      }
-      accessory.sendRequest("multipleFieldSet", {"remoteCommand": "dock"}, function(err, httpResponse, body) {
-        if (err || httpResponse.statusCode != 200) {
-          accessory.log("Could not dock Roomba: %s", err);
-          callback(err);
-          return;
-        }
-        callback();
-      });
-    });
-  });
-}
-
-RoombaAccessory.prototype.start = function(callback) {
-  this.sendRequest("multipleFieldSet", { "remoteCommand": "start" }, function(err, httpResponse, body) {
-    if (err || httpResponse.statusCode != 200) {
-      accessory.log("Could not start Roomba: %s", err);
-      return callback(err);
-    }
-    callback();
-  });
-}
-
 RoombaAccessory.prototype.isRunning = function(callback) {
-  this.getStatus(function(err, theStatus) {
-    if (err) {
-      return callback(err);
-    }
-    return callback(null, theStatus.robot_status.phase == "run")
-  });
+  this.roomba.isRunning(callback);
 }
 
 RoombaAccessory.prototype.setPowerState = function(powerOn, callback) {
   if (powerOn) {
-    this.start(callback);
+    this.roomba.start(callback);
   } else {
-    this.pauseAndDock(callback);
+    this.roomba.pauseAndDock(callback);
   }
 }
